114ed9fe4a863c2a12d3dac552f95ef226ad5a16
yassine315 <yassim.lmahi@gmail.com>
2019-10-18 01:54:28 +0200

    suprimer le main

--- src/testPOO/TestConjoint.java
+++ src/testPOO/TestConjoint.java
@@ -5,19 +5,7 @@ public class TestConjoint {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
  
-		Personne Y = new Personne("EL-Mahi","Yassine",Sex.homme);
-		Personne N = new Personne();
-		
-		try {
-			Y.marier(N);
-		} catch (MemeSex e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (DejaMariee e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 	
-	System.out.println(Y.toString()+"est marié à"+N.toString());
+		
 	}
 }
